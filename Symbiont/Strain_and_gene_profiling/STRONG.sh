573fe1dbe265e057465b19ff1f451e27667cbd61
cbreusing <77281303+cbreusing@users.noreply.github.com>
2023-05-18 14:04:00 -0400

    Add files via upload

--- Symbiont/Strain_and_gene_profiling/STRONG.sh
+++ Symbiont/Strain_and_gene_profiling/STRONG.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+#SBATCH -J STRONG
+#SBATCH -t 96:00:00
+#SBATCH -n 32
+#SBATCH -N 1
+#SBATCH -p bigmem
+#SBATCH --mem=1t
+#SBATCH -o STRONG.out
+#SBATCH -e STRONG.err
 
+source activate STRONG
+
+STRONG --config config.yaml --threads 32 --verbose STRONG
