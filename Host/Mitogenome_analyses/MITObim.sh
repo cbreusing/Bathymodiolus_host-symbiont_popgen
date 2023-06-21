f15606a11cd50f987ecfa10ead00e2faf42e3bed
cbreusing <77281303+cbreusing@users.noreply.github.com>
2023-06-21 15:07:06 -0400

    Add files via upload

--- Host/Mitogenome_analyses/MITObim.sh
+++ Host/Mitogenome_analyses/MITObim.sh
@@ -4,19 +4,20 @@
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH --mem=10g
-#SBATCH -o MITObim_J2432_B%a.out
-#SBATCH -e MITObim_J2432_B%a.err
+#SBATCH -o MITObim_%a.out
+#SBATCH -e MITObim_%a.err
 #SBATCH --array=238-257
 
 module load perl
 module load mira
 module load bbmap
 
-# Examples are shown based on Tow Cam population J2432
-reformat.sh in1=J2432_B${SLURM_ARRAY_TASK_ID}_R1_clean.fastq in2=J2432_B${SLURM_ARRAY_TASK_ID}_R2_clean.fastq out=J2432_B${SLURM_ARRAY_TASK_ID}_interleaved.fastq
-mkdir J2432_B${SLURM_ARRAY_TASK_ID}_mtDNA
-cd J2432_B${SLURM_ARRAY_TASK_ID}_mtDNA
-/gpfs/data/rbeinart/Software/MITObim/MITObim.pl -start 1 -end 30 -sample J2432_B${SLURM_ARRAY_TASK_ID} -ref B_septemdierum_mitogenome -readpool ../J2432_B${SLURM_ARRAY_TASK_ID}_interleaved.fastq --quick ../B_septemdierum_mitogenome.fasta --clean
+FILE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" filelist.txt)
+
+reformat.sh in1=${FILE}_R1_clean.fastq in2=${FILE}_R2_clean.fastq out=${FILE}_interleaved.fastq
+mkdir ${FILE}_mtDNA
+cd ${FILE}_mtDNA
+/gpfs/data/rbeinart/Software/MITObim/MITObim.pl -start 1 -end 30 -sample ${FILE} -ref B_septemdierum_mitogenome -readpool ../${FILE}_interleaved.fastq --quick ../B_septemdierum_mitogenome.fasta --clean
 
 
 
--- Host/Mitogenome_analyses/mtVariantCalling.sh
+++ Host/Mitogenome_analyses/mtVariantCalling.sh
@@ -4,9 +4,9 @@
 #SBATCH -n 24
 #SBATCH -N 1
 #SBATCH --mem=50g
-#SBATCH -o mtVariantCalling_J2432_B%a.out
-#SBATCH -e mtVariantCalling_J2432_B%a.err
-#SBATCH --array=238-257
+#SBATCH -o mtVariantCalling_%a.out
+#SBATCH -e mtVariantCalling_%a.err
+#SBATCH --array=1-105
 
 source activate lofreq
 module load bowtie2/2.3.5.1
@@ -17,24 +17,25 @@ module load R
 module load vcftools
 module load bcftools
 
-# Examples are shown based on Tow Cam population J2432
-bowtie2-build J2432_B${SLURM_ARRAY_TASK_ID}_mitogenome.fasta J2432_B${SLURM_ARRAY_TASK_ID}_mitogenome.fasta
-samtools faidx J2432_B${SLURM_ARRAY_TASK_ID}_mitogenome.fasta
+FILE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" filelist.txt)
 
-bowtie2 --very-sensitive -p 24 -x J2432_B${SLURM_ARRAY_TASK_ID}_mitogenome.fasta -1 ../J2432_B${SLURM_ARRAY_TASK_ID}_R1_clean.fastq -2 ../J2432_B${SLURM_ARRAY_TASK_ID}_R2_clean.fastq | samtools view -bS -h -@ 24 - | samtools sort -@ 24 - > J2432_B${SLURM_ARRAY_TASK_ID}.mt.sorted.bam
-java -Xmx2g -jar /gpfs/runtime/opt/picard-tools/2.17.11/picard.jar MarkDuplicates I=J2432_B${SLURM_ARRAY_TASK_ID}.mt.sorted.bam O=J2432_B${SLURM_ARRAY_TASK_ID}.mt.dedup.bam M=J2432_B${SLURM_ARRAY_TASK_ID}.mt.metrics.txt REMOVE_DUPLICATES=true
-samtools index J2432_B${SLURM_ARRAY_TASK_ID}.mt.dedup.bam
-lofreq viterbi -f J2432_B${SLURM_ARRAY_TASK_ID}_mitogenome.fasta -k J2432_B${SLURM_ARRAY_TASK_ID}.mt.dedup.bam | samtools sort -@ 24 - > J2432_B${SLURM_ARRAY_TASK_ID}.mt.realigned.bam
-lofreq indelqual -f J2432_B${SLURM_ARRAY_TASK_ID}_mitogenome.fasta --dindel -o J2432_B${SLURM_ARRAY_TASK_ID}.mt.indelqual.bam J2432_B${SLURM_ARRAY_TASK_ID}.mt.realigned.bam
-samtools index J2432_B${SLURM_ARRAY_TASK_ID}.mt.indelqual.bam
-samtools view -bS -h -F4 J2432_B${SLURM_ARRAY_TASK_ID}.mt.indelqual.bam > J2432_B${SLURM_ARRAY_TASK_ID}.filt.mt.dedup.bam
-java -Xmx2g -jar /gpfs/runtime/opt/picard-tools/2.17.11/picard.jar AddOrReplaceReadGroups I=J2432_B${SLURM_ARRAY_TASK_ID}.filt.mt.dedup.bam O=J2432_B${SLURM_ARRAY_TASK_ID}.filt.mt.dedup.RG.bam RGID=J2432_B${SLURM_ARRAY_TASK_ID} RGLB=LIB_J2432_B${SLURM_ARRAY_TASK_ID} RGPL=ILLUMINA RGPU=FLOWCELL1 RGSM=J2432_B${SLURM_ARRAY_TASK_ID} VALIDATION_STRINGENCY=SILENT
+bowtie2-build ${FILE}_mitogenome.fasta ${FILE}_mitogenome.fasta
+samtools faidx ${FILE}_mitogenome.fasta
 
-lofreq call --call-indels -q 20 -Q 20 -m 30 -f J2432_B${SLURM_ARRAY_TASK_ID}_mitogenome.fasta -o J2432_B${SLURM_ARRAY_TASK_ID}.mt.LoFreq.vcf J2432_B${SLURM_ARRAY_TASK_ID}.filt.mt.dedup.RG.bam
+bowtie2 --very-sensitive -p 24 -x ${FILE}_mitogenome.fasta -1 ../${FILE}_R1_clean.fastq -2 ../${FILE}_R2_clean.fastq | samtools view -bS -h -@ 24 - | samtools sort -@ 24 - > ${FILE}.mt.sorted.bam
+java -Xmx2g -jar /gpfs/runtime/opt/picard-tools/2.17.11/picard.jar MarkDuplicates I=${FILE}.mt.sorted.bam O=${FILE}.mt.dedup.bam M=${FILE}.mt.metrics.txt REMOVE_DUPLICATES=true
+samtools index ${FILE}.mt.dedup.bam
+lofreq viterbi -f ${FILE}_mitogenome.fasta -k ${FILE}.mt.dedup.bam | samtools sort -@ 24 - > ${FILE}.mt.realigned.bam
+lofreq indelqual -f ${FILE}_mitogenome.fasta --dindel -o ${FILE}.mt.indelqual.bam ${FILE}.mt.realigned.bam
+samtools index ${FILE}.mt.indelqual.bam
+samtools view -bS -h -F4 ${FILE}.mt.indelqual.bam > ${FILE}.filt.mt.dedup.bam
+java -Xmx2g -jar /gpfs/runtime/opt/picard-tools/2.17.11/picard.jar AddOrReplaceReadGroups I=${FILE}.filt.mt.dedup.bam O=${FILE}.filt.mt.dedup.RG.bam RGID=${FILE} RGLB=LIB_${FILE} RGPL=ILLUMINA RGPU=FLOWCELL1 RGSM=${FILE} VALIDATION_STRINGENCY=SILENT
 
-cat J2432_B${SLURM_ARRAY_TASK_ID}.mt.LoFreq.vcf | vcf-sort -c > J2432_B${SLURM_ARRAY_TASK_ID}.mt.LoFreq.sorted.vcf
-bgzip -c J2432_B${SLURM_ARRAY_TASK_ID}.mt.LoFreq.sorted.vcf > J2432_B${SLURM_ARRAY_TASK_ID}.mt.LoFreq.vcf.gz
-tabix -p vcf J2432_B${SLURM_ARRAY_TASK_ID}.mt.LoFreq.vcf.gz
-bcftools filter -g 5 -o J2432_B${SLURM_ARRAY_TASK_ID}.mt.LoFreq.filtered.vcf -O v J2432_B${SLURM_ARRAY_TASK_ID}.mt.LoFreq.vcf.gz
-vcftools --vcf J2432_B${SLURM_ARRAY_TASK_ID}.mt.LoFreq.filtered.vcf --max-meanDP 1000 --recode --recode-INFO-all --out J2432_B${SLURM_ARRAY_TASK_ID}.mt.LoFreq.FINAL
+lofreq call --call-indels -q 20 -Q 20 -m 30 -f ${FILE}_mitogenome.fasta -o ${FILE}.mt.LoFreq.vcf ${FILE}.filt.mt.dedup.RG.bam
+
+cat ${FILE}.mt.LoFreq.vcf | vcf-sort -c > ${FILE}.mt.LoFreq.sorted.vcf
+bgzip -c ${FILE}.mt.LoFreq.sorted.vcf > ${FILE}.mt.LoFreq.vcf.gz
+tabix -p vcf ${FILE}.mt.LoFreq.vcf.gz
+bcftools filter -g 5 -o ${FILE}.mt.LoFreq.filtered.vcf -O v ${FILE}.mt.LoFreq.vcf.gz
+vcftools --vcf ${FILE}.mt.LoFreq.filtered.vcf --max-meanDP 1000 --recode --recode-INFO-all --out ${FILE}.mt.LoFreq.FINAL
 
